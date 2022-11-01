9abf5d545339501ba507f443634dc94fa464ceee
Vashudev Dhama <vashudevdhama@gofynd.com>
2022-11-01 07:08:28 +0530

    binding action creators useActions()

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,22 +1,21 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../redux';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState<string>('');
-  const dispatch = useDispatch();
+  const { searchRepositories } = useActions();
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch(actionCreators.searchRepositories(term) as any);
-  }
+    searchRepositories(term);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type='text' onChange={(e) => setTerm(e.target.value)}/>
+        <input type='text' onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
   );
 };
 
-export default RepositoriesList; 
+export default RepositoriesList;
