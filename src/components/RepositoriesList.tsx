b58d587f083ef1e59c5f00f3641b062d31543b90
Vashudev Dhama <vashudevdhama@gofynd.com>
2022-11-01 06:52:55 +0530

    wiring up redux to react

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RepositoriesList: React.FC = () => {
+  return (
+    <div>
+      <form>
+        <input type='text' />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList; 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import RepositoriesList from './components/RepositoriesList';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 const App = () => {
-  return <div>Hello there</div>;
+  return (
+    <Provider store={store}>
+      <div>Hello there</div>
+      <RepositoriesList />
+    </Provider>
+  );
 };
 
 root.render(
